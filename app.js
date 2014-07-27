5736ce2e33523ef6c3ba5474212abfb52b63e6f2
Alessandra Pearson <alessandra.pearson@fracturedatlas.org>
2014-07-27 22:04:17 +0000

    made my site

--- app.js
+++ app.js
@@ -5,10 +5,36 @@ appHelper.runApp(function(app, db) {
 
 	console.log("Travel application is running!");
 
-	var myName = "Alessandra";
-	app.get("/", function (req, resp) {
-		resp.write("<html><body><h1>Welcome to "+myName+"'s first web app!</body></html>");
-		resp.end();
-	});
+  var myName = "Alessandra";
+  app.get("/", function (req, resp) {
+    var obj = {
+      "name" : myName, 
+      "food" : "pasta"
+    }
+    resp.render("index", obj);
+  });
+  
+  app.get("/icecream/:flavor/:topping", function (req,resp){
+    resp.render("icecream", {flavor: req.params.flavor, topping: req.params.topping });
+  });
 
-})
+  app.get("/travel", function(req, resp) { 
+    db.findArray({}, function (results) {
+      var params = {
+        "places": results
+    }
+   
+    resp.render("travel", params);
+  })
+  })
+  app.get("/travel/rating/:rating", function(req, resp) { 
+    var rating = parseInt(req.params.rating);
+    db.findArray({"rating" : rating }, function (results) {
+      var params = {
+        "places": results
+    }
+   
+    resp.render("travel", params);
+  })
+  })
+});
